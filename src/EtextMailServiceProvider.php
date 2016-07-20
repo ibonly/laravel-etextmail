961eec46fd9872c41b700a2adb265655c5b1815b
Adeniyi Ibraheem <ibonly01@gmail.com>
2016-07-20 12:14:36 +0100

    Update service provider

--- src/EtextMailServiceProvider.php
+++ src/EtextMailServiceProvider.php
@@ -1,22 +1,58 @@
-<?phps
+<?php
 
-namespace Ibonly\EtextMail;
+/*
+ * This file is part of the Laravel etextmai, package.
+ *
+ * (c) Ibraheem Adeniyi <ibonly01@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Unicodeveloper\EtextMail;
 
 use Illuminate\Support\ServiceProvider;
 
-class EtextMailServiceProvider extends ServiceProvider
+class PaystackServiceProvider extends ServiceProvider
 {
-	protected $defer = false;
 
-	public function register()
-	{
-		$this->app->bind('laravel-etextmail', function() {
-			return new EtextMail;
-		});
-	}
+    /*
+    * Indicates if loading of the provider is deferred.
+    *
+    * @var bool
+    */
+    protected $defer = false;
 
-	public function provides()
-	{
-		return ['laravel-etextmail'];
-	}
+    /**
+    * Publishes all the config file this package needs to function
+    */
+    public function boot()
+    {
+        $config = realpath(__DIR__.'/../resources/config/etextmail.php');
+
+        $this->publishes([
+            $config => config_path('etextmail.php')
+        ]);
+    }
+
+    /**
+    * Register the application services.
+    */
+    public function register()
+    {
+        $this->app->bind('laravel-etextmail', function () {
+
+            return new EtextMail;
+
+        });
+    }
+
+    /**
+    * Get the services provided by the provider
+    * @return array
+    */
+    public function provides()
+    {
+        return ['laravel-etextmail'];
+    }
 }
