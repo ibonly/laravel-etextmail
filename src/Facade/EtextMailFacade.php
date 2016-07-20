7df93488a55b3609ba5a8e62b33666d36853c256
Adeniyi Ibraheem <ibonly01@gmail.com>
2016-07-20 12:33:23 +0100

    Update facades

---
+++ src/Facade/EtextMailFacade.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ibonly\EtextMail\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class EtextMailFacade extends Facade
+{
+	protected static function getFacadeAccessor()
+	{
+		return 'laravel-etextmail';
+	}
+}
