977dbe87b0d1a701600efa0a562e01341e289937
Adeniyi Ibraheem <ibonly01@gmail.com>
2016-07-28 23:28:48 +0100

    Update codeclimate-ci issues

--- src/EtextMail.php
+++ src/EtextMail.php
@@ -188,7 +188,7 @@ class EtextMail
      * @param  $sData
      * @return string      
      */
-    public function queryString($sData)
+    private function queryString($sData)
     {
         $data = array();
 
@@ -205,7 +205,7 @@ class EtextMail
      * @param  $url
      * @return string
      */
-    public function parseUrl($url)
+    private function parseUrl($url)
     {
         $url = parse_url($url);
         if ($url['scheme'] != 'http') {
@@ -222,21 +222,21 @@ class EtextMail
      * @param  $sData
      * @return resource
      */
-    public function sendRequest($url, $sData)
+    private function sendRequest($url, $sData)
     {
         $data = $this->queryString($sData);
         $host = $this->parseUrl($url)['host'];                                    // extract host and path:
         $path = $this->parseUrl($url)['path'];
-        $fp = fsockopen($host, 80);                                               // open a socket connection on port 80
+        $socket = fsockopen($host, 80);                                               // open a socket connection on port 80
      
-        fputs($fp, "POST $path HTTP/1.1\r\n");                                    // send the request headers:
-        fputs($fp, "Host: $host\r\n");
-        fputs($fp, "Content-type: application/x-www-form-urlencoded\r\n");
-        fputs($fp, "Content-length: ". strlen($data) ."\r\n");
-        fputs($fp, "Connection: close\r\n\r\n");
-        fputs($fp, $data);
+        fputs($socket, "POST $path HTTP/1.1\r\n");                                    // send the request headers:
+        fputs($socket, "Host: $host\r\n");
+        fputs($socket, "Content-type: application/x-www-form-urlencoded\r\n");
+        fputs($socket, "Content-length: ". strlen($data) ."\r\n");
+        fputs($socket, "Connection: close\r\n\r\n");
+        fputs($socket, $data);
 
-        return $fp;
+        return $socket;
     }
 
     /**
@@ -246,16 +246,16 @@ class EtextMail
      * @param  $sData
      * @return array
      */
-    public function postRequest($url, $sData) 
+    private function postRequest($url, $sData) 
     {
-        $fp = $this->sendRequest($url, $sData);
+        $socket = $this->sendRequest($url, $sData);
 
         $result = ''; 
-        while (!feof($fp)) {
-            $result .= fgets($fp, 128);
+        while (!feof($socket)) {
+            $result .= fgets($socket, 128);
         }
 
-        fclose($fp);
+        fclose($socket);
      
         $result = explode("\r\n\r\n", $result, 2);                                // split the result header from the content
         $header = isset($result[0]) ? $result[0] : '';
@@ -267,7 +267,7 @@ class EtextMail
     /**
      * @param string $senderId
      */
-    public function validateSenderId($senderId)
+    private function validateSenderId($senderId)
     {
         return strlen($senderId) <= 11 != 0 && strlen($senderId) >= 2 ? true : false;
     }
@@ -279,19 +279,19 @@ class EtextMail
      * @param  $data
      * @return string
      */
-    public function getResponse($url, $data)
+    private function getResponse($url, $data)
     {
         list($header, $content) = $this->postRequest($url, $data);
 
         $tok = strtok($content, " "); //Split the $content result into words
 
-        $error_code = explode(' ', $content)[1];
+        $errorCode = explode(' ', $content)[1];
 
         if (!$this->validateSenderId($this->getSenderId())) {
-            throw new EtextMailException($error_code);
+            throw new EtextMailException($errorCode);
         }
 
-        return $this->successErrorMessage($tok, $error_code);
+        return $this->successErrorMessage($tok, $errorCode);
     }
 
     /**
@@ -301,13 +301,13 @@ class EtextMail
      * @param  $errorCode
      * @return string
      */
-    public function successErrorMessage($tok, $errorCode)
+    private function successErrorMessage($tok, $errorCode)
     {
         if ($tok == "OK") {
             $tok = strtok(" ");
             return $tok;
-        } else {
-            throw new EtextMailException($errorCode);
         }
+        
+        throw new EtextMailException($errorCode);
     }
 }
