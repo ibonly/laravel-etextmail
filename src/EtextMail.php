d9121adbf6149a12a557d3700f667f04308df7c9
Adeniyi Ibraheem <ibonly01@gmail.com>
2016-08-23 13:35:51 +0100

    Update code quality

--- src/EtextMail.php
+++ src/EtextMail.php
@@ -2,101 +2,11 @@
 
 namespace Ibonly\EtextMail;
 
-use Illuminate\Support\Facades\Config;
+use Ibonly\EtextMail\Helpers\BaseController;
 use Ibonly\EtextMail\Exception\EtextMailException;
 
-class EtextMail
+class EtextMail extends BaseController
 {
-    /**
-     * Get senderid from environment variable
-     * 
-     * @return string
-     */
-    public function getSenderId()
-    {
-        return Config::get('etextmail.senderid');
-    }
-
-    /**
-     * get username from environment variables
-     * 
-     * @return string
-     */
-    public function getUsername()
-    {
-        return Config::get('etextmail.username');
-    }
-
-    /**
-     * get password from environment variables
-     * 
-     * @return string
-     */
-    public function getPassword()
-    {
-        return Config::get('etextmail.password');
-    }
-
-    /**
-     * get url from environment variables
-     * 
-     * @return string
-     */
-    public function getDomain()
-    {
-        return Config::get('etextmail.url');
-    }
-
-    /**
-     * Set the data required to get credit balance
-     * 
-     * @return array
-     */
-    public function setBalanceData()
-    {
-        return [
-                'UN' => $this->getUsername(), 
-                'p'  => $this->getPassword()
-            ];
-    }
-
-    /**
-     * Set the data required to send sms
-     * 
-     * @param  $destination
-     * @param  $message
-     * @param  $long
-     * @return array
-     */
-    public function setSendData($destination, $message, $long)
-    {
-        $longSms = $long === null ? 0 : $long;
-
-        return [     
-                'UN' => $this->getUsername(), 
-                'p'  => $this->getPassword(),
-                'SA' => $this->getSenderId(),
-                'DA' => $destination,
-                'L'  => $longSms, 
-                'M'  => $message
-            ];
-    }
-
-    /**
-     * Set the data required to get message details
-     * 
-     * @param  $message
-     * @return array
-     */
-    public function setMessageCountData($message)
-    {
-        return [
-                'UN' => $this->getUsername(), 
-                'p'  => $this->getPassword(),
-                'M'  => $message
-            ];
-    }
-
     /**
      * Build sms send api url
      * 
